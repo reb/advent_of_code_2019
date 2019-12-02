ea652f790dbaa61343dc2106c561764d8d6998f8
reb <reboumans@gmail.com>
2019-12-02 15:08:12 -0500

    Execute intcode program and add tests for given testcases

--- src/day_02.rs
+++ src/day_02.rs
@@ -75,10 +75,88 @@
 /// fire. To do this, before running the program, replace position 1 with the value 12 and replace
 /// position 2 with the value 2. What value is left at position 0 after the program halts?
 
-
 const INPUT: &str = include_str!("../input/day_02.txt");
 
 pub fn run() {
-    println!("Not implemented yet");
-    unimplemented!();
+    let mut program = get_input();
+
+    // reproduce the "1202 program alarm" by setting position 1 & 2
+    program[1] = 12;
+    program[2] = 2;
+
+    program = execute_intcode(program);
+    println!("The value left at position 0 after reproducing the \"1202 program alarm\" is: {}", program[0]);
+
+}
+
+fn execute_intcode(mut program: Vec<usize>) -> Vec<usize> {
+    let mut position = 0;
+    loop {
+        match &program[position..] {
+            &[99, ..] => break,
+            &[1, first, second, target, ..] => {
+                program[target] = program[first] + program[second];
+            },
+            &[2, first, second, target, ..] => {
+                program[target] = program[first] * program[second];
+            },
+            _ => panic!("Unknown instruction"),
+
+        }
+        position += 4;
+    }
+    program
+}
+
+fn get_input() -> Vec<usize> {
+    INPUT.trim()
+        .split(',')
+        .map(|number| number.parse())
+        .filter_map(Result::ok)
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_execute_intcode_1() {
+        let input = vec![1,0,0,0,99];
+        let output = vec![2,0,0,0,99];
+
+        assert_eq!(execute_intcode(input), output);
+    }
+
+    #[test]
+    fn test_execute_intcode_2() {
+        let input = vec![2,3,0,3,99];
+        let output = vec![2,3,0,6,99];
+
+        assert_eq!(execute_intcode(input), output);
+    }
+
+    #[test]
+    fn test_execute_intcode_3() {
+        let input = vec![2,4,4,5,99,0];
+        let output = vec![2,4,4,5,99,9801];
+
+        assert_eq!(execute_intcode(input), output);
+    }
+
+    #[test]
+    fn test_execute_intcode_4() {
+        let input = vec![1,1,1,4,99,5,6,0,99];
+        let output = vec![30,1,1,4,2,5,6,0,99];
+
+        assert_eq!(execute_intcode(input), output);
+    }
+
+    #[test]
+    fn test_execute_intcode_5() {
+        let input = vec![1,9,10,3,2,3,11,0,99,30,40,50];
+        let output = vec![3500,9,10,70,2,3,11,0,99,30,40,50];
+
+        assert_eq!(execute_intcode(input), output);
+    }
 }
