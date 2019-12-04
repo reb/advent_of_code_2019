7dce3f77392da98ed8d2c22838fe3279c2e6bbb2
reb <reboumans@gmail.com>
2019-12-04 17:59:02 -0500

    Test validity of potential passwords in given range

--- src/day_04.rs
+++ src/day_04.rs
@@ -23,7 +23,90 @@
 ///
 /// Your puzzle input is 264360-746325.
 
+struct Digits {
+    number: u32,
+    divisor: u32,
+}
+
+impl Digits {
+    fn new(number: u32) -> Self {
+        let mut divisor = 1;
+        while number / divisor >= 10 {
+            divisor *= 10;
+        }
+
+        Digits {
+            number: number,
+            divisor: divisor,
+        }
+    }
+}
+
+impl Iterator for Digits {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.divisor == 0 {
+            None
+        } else {
+            let next_digit = Some(self.number / self.divisor);
+            self.number %= self.divisor;
+            self.divisor /= 10;
+            next_digit
+        }
+    }
+}
+
 pub fn run() {
-    println!("Not implemented yet");
-    unimplemented!();
+    let start = 264360;
+    let end = 746325;
+
+    let mut valid_passwords = 0;
+    for number in start..=end {
+        if is_valid(number) {
+            valid_passwords += 1;
+        }
+    }
+    println!("Amount of passwords meeting the criteria in the given range: {}", valid_passwords);
+}
+
+fn is_valid(number: u32) -> bool {
+    let mut iter = Digits::new(number).peekable();
+    let mut two_same_adjacent = false;
+    while let Some(digit) = iter.next() {
+        if let Some(next_digit) = iter.peek() {
+            if next_digit < &digit {
+                return false;
+            }
+            if next_digit == &digit {
+                two_same_adjacent = true;
+            }
+        }
+    }
+
+    // all digits increase in value, validity depends on two same adjacent digits
+    two_same_adjacent
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_valid_1() {
+        let input = 111111;
+        assert!(is_valid(input));
+    }
+
+    #[test]
+    fn test_is_valid_2() {
+        let input = 223450;
+        assert!(!is_valid(input));
+    }
+
+    #[test]
+    fn test_is_valid_3() {
+        let input = 123789;
+        assert!(!is_valid(input));
+    }
 }
