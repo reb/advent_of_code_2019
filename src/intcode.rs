0d2382be052a16759faf0569e1f414efc9bdb369
reb <reboumans@gmail.com>
2019-12-06 21:59:17 -0500

    Reformat using rustfmt

--- src/intcode.rs
+++ src/intcode.rs
@@ -9,30 +9,28 @@ pub fn execute(mut program: Vec<i32>, mut inputs: Vec<i32>) -> (Vec<i32>, Vec<i3
     let mut outputs: Vec<i32> = Vec::new();
     loop {
         let (modes, opcode) = extract_modes(program[position]);
-        let parameters = &program[position+1..];
+        let parameters = &program[position + 1..];
 
         match (opcode, parameters) {
             (99, _) => break,
             (1, &[first, second, write, ..]) => {
-                program[write as usize] =
-                    find_value(first, &(modes[0]), &program) +
-                    find_value(second, &(modes[1]), &program);
+                program[write as usize] = find_value(first, &(modes[0]), &program)
+                    + find_value(second, &(modes[1]), &program);
                 position += 4;
-            },
+            }
             (2, &[first, second, write, ..]) => {
-                program[write as usize] =
-                    find_value(first, &(modes[0]), &program) *
-                    find_value(second, &(modes[1]), &program);
+                program[write as usize] = find_value(first, &(modes[0]), &program)
+                    * find_value(second, &(modes[1]), &program);
                 position += 4;
-            },
+            }
             (3, &[write, ..]) => {
                 program[write as usize] = inputs.remove(0);
                 position += 2;
-            },
+            }
             (4, &[read, ..]) => {
                 outputs.push(find_value(read, &modes[0], &program));
                 position += 2;
-            },
+            }
             invalid => panic!("Unknown instruction: {:?}", invalid),
         }
     }
@@ -64,7 +62,8 @@ fn find_value(number: i32, mode: &Mode, program: &Vec<i32>) -> i32 {
 }
 
 pub fn load(input: &str) -> Vec<i32> {
-    input.trim()
+    input
+        .trim()
         .split(',')
         .map(|number| number.parse())
         .filter_map(Result::ok)
@@ -77,60 +76,57 @@ mod tests {
 
     #[test]
     fn test_execute_1() {
-        let input = vec![1,0,0,0,99];
-        let output = vec![2,0,0,0,99];
+        let input = vec![1, 0, 0, 0, 99];
+        let output = vec![2, 0, 0, 0, 99];
 
         assert_eq!(execute(input, Vec::new()), (output, Vec::new()));
     }
 
     #[test]
     fn test_execute_2() {
-        let input = vec![2,3,0,3,99];
-        let output = vec![2,3,0,6,99];
+        let input = vec![2, 3, 0, 3, 99];
+        let output = vec![2, 3, 0, 6, 99];
 
         assert_eq!(execute(input, Vec::new()), (output, Vec::new()));
     }
 
     #[test]
     fn test_execute_3() {
-        let input = vec![2,4,4,5,99,0];
-        let output = vec![2,4,4,5,99,9801];
+        let input = vec![2, 4, 4, 5, 99, 0];
+        let output = vec![2, 4, 4, 5, 99, 9801];
 
         assert_eq!(execute(input, Vec::new()), (output, Vec::new()));
     }
 
     #[test]
     fn test_execute_4() {
-        let input = vec![1,1,1,4,99,5,6,0,99];
-        let output = vec![30,1,1,4,2,5,6,0,99];
+        let input = vec![1, 1, 1, 4, 99, 5, 6, 0, 99];
+        let output = vec![30, 1, 1, 4, 2, 5, 6, 0, 99];
 
         assert_eq!(execute(input, Vec::new()), (output, Vec::new()));
     }
 
     #[test]
     fn test_execute_5() {
-        let input = vec![1,9,10,3,2,3,11,0,99,30,40,50];
-        let output = vec![3500,9,10,70,2,3,11,0,99,30,40,50];
+        let input = vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50];
+        let output = vec![3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50];
 
         assert_eq!(execute(input, Vec::new()), (output, Vec::new()));
     }
 
     #[test]
     fn test_execute_opcode_3_and_4() {
-        let input_program = vec![3,0,4,0,99];
+        let input_program = vec![3, 0, 4, 0, 99];
         let inputs = vec![1];
-        let output_program = vec![1,0,4,0,99];
+        let output_program = vec![1, 0, 4, 0, 99];
         let outputs = vec![1];
-        assert_eq!(
-            execute(input_program, inputs),
-            (output_program, outputs)
-        );
+        assert_eq!(execute(input_program, inputs), (output_program, outputs));
     }
 
     #[test]
     fn test_execute_modes() {
-        let input = vec![1002,4,3,4,33];
-        let output = vec![1002,4,3,4,99];
+        let input = vec![1002, 4, 3, 4, 33];
+        let output = vec![1002, 4, 3, 4, 99];
 
         assert_eq!(execute(input, Vec::new()), (output, Vec::new()));
     }
@@ -145,7 +141,7 @@ mod tests {
 
     #[test]
     fn test_find_value_position_mode() {
-        let program = &vec![1002,4,3,4,33];
+        let program = &vec![1002, 4, 3, 4, 33];
         let mode = &Mode::Position;
         let number = 4;
 
@@ -155,7 +151,7 @@ mod tests {
 
     #[test]
     fn test_find_value_immediate_mode() {
-        let program = &vec![1002,4,3,4,33];
+        let program = &vec![1002, 4, 3, 4, 33];
         let mode = &Mode::Immediate;
         let number = 3;
 
