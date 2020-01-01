1a5d90208382b1fe7a0759d3c46f97014ece0279
reb <reboumans@gmail.com>
2020-01-01 15:56:59 -0500

    Add sensor boost problem

--- src/main.rs
+++ src/main.rs
@@ -53,4 +53,6 @@ macro_rules! modules {
     };
 }
 
-modules![day_01, day_02, day_03, day_04, day_05, day_06, day_07, day_08,];
+modules![
+    day_01, day_02, day_03, day_04, day_05, day_06, day_07, day_08, day_09,
+];
