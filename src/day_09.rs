666ae5717c718edbff82329a9554491832f6c0d6
reb <reboumans@gmail.com>
2020-01-03 12:29:24 -0500

    Detail how to run the BOOST program

--- src/day_09.rs
+++ src/day_09.rs
@@ -64,6 +64,22 @@
 /// report no malfunctioning opcodes when run in test mode; it should only
 /// output a single value, the BOOST keycode. What BOOST keycode does it
 /// produce?
+///
+/// --- Part Two ---
+///
+/// You now have a complete Intcode computer.
+///
+/// Finally, you can lock on to the Ceres distress signal! You just need to
+/// boost your sensors using the BOOST program.
+///
+/// The program runs in sensor boost mode by providing the input instruction the
+/// value 2. Once run, it will boost the sensors automatically, but it might
+/// take a few seconds to complete the operation on slower hardware. In sensor
+/// boost mode, the program will output a single value: the coordinates of the
+/// distress signal.
+///
+/// Run the BOOST program in sensor boost mode. What are the coordinates of the
+/// distress signal?
 use intcode;
 
 const INPUT: &str = include_str!("../input/day_09.txt");
